cc57b881ebd8771b21ab8943714770f513a3ad09
Ale Farias <mail.alefarias@gmail.com>
2020-08-17 21:35:01 -0300

    Some updates on KMeans

--- clustering/KMeans.jl
+++ clustering/KMeans.jl
@@ -7,15 +7,18 @@ using Random
 # Vamos definir a média e a variância dos dois grupos
 const μ_group_a = 20
 const μ_group_b = 30
+const μ_group_c = -10
 const σ = 1.5
 # Instanciamos o tipo de distrbuição
 norm_a = Normal(μ_group_a, σ)
 norm_b = Normal(μ_group_b, σ)
+norm_c = Normal(μ_group_c, σ)
 # Finalmente geramos os números para as distribuições instanciadas anteriormente
 A = rand(norm_a, 100000, 2)
 B = rand(norm_b, 100000, 2)
+C = rand(norm_c, 100000, 2)
 # A função vcat faz a concatenação vertical das matrizes
-X = vcat(A, B)
+X = vcat(A, B, C)
 
 # Funções auxiliares
 random_index(X) = sample(axes(X, 1))
@@ -55,12 +58,11 @@ function compute_inertia(old_centroids, new_centroids, n_centroids)
     return new_inertia
 end
 
-function kmeans(n_groups)
+function kmeans(n_groups::Int8)
     centroids = [X[random_index(X), :] for i = 1:n_groups]
     temp_centroids_vectors = []
     inertia = Inf
 
-    # Loop
     while inertia ≥ 0.01
         empty!(temp_centroids_vectors)
         for i=1:n_groups
@@ -70,6 +72,7 @@ function kmeans(n_groups)
         for i=axes(X, 1)
             find_closest_centroid!(X[i, :], centroids, temp_centroids_vectors)
         end
+
         new_centroids = compute_new_centroids(temp_centroids_vectors, n_groups)
         inertia = compute_inertia(centroids, new_centroids, n_groups)
         centroids = new_centroids
@@ -78,5 +81,7 @@ function kmeans(n_groups)
     return centroids
 end
 
-final_centroids = kmeans(3)
-println("\nFinal centroids:\n", final_centroids)
+final_centroids = kmeans(Int8(3))
+println("\nFinal centroids:\n")
+display(final_centroids)
+println()
